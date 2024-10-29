29ac5410381a565659d8df1b211be4acc87b9943
bearl <125728897+bearl27@users.noreply.github.com>
2024-10-29 15:44:43 +0900

    DemoにJump機能を足した！ いまはSpaceでジャンプあとで直して

---
+++ museum/Assets/cs/Camera/CameraJump.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class CameraJump : MonoBehaviour
+{
+    public OVRCameraRig cameraRig;
+
+    private bool isJumping = false;
+
+    // ジャンプ関連のパラメータ
+    private float jumpHeight = 1.5f;
+    private float jumpDuration = 0.5f;
+    private float jumpStartTime;
+    private float initialHeight;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && !OVRToRoom.isEnterPicture && !OVRToStreet.isToStreet)
+        {
+            StartJump();
+        }
+
+        if (isJumping)
+        {
+            UpdateJump();
+        }
+    }
+
+    private void StartJump()
+    {
+        isJumping = true;
+        jumpStartTime = Time.time;
+        initialHeight = cameraRig.transform.position.y;
+    }
+
+    private void UpdateJump()
+    {
+        float elapsedTime = Time.time - jumpStartTime;
+        float progress = elapsedTime / jumpDuration;
+
+        if (progress < 1f)
+        {
+            // 放物線的なジャンプの動き
+            float height = initialHeight + jumpHeight * Mathf.Sin(progress * Mathf.PI);
+            cameraRig.transform.position = new Vector3(
+                cameraRig.transform.position.x,
+                height,
+                cameraRig.transform.position.z
+            );
+        }
+        else
+        {
+            // ジャンプ終了時
+            cameraRig.transform.position = new Vector3(
+                cameraRig.transform.position.x,
+                initialHeight,
+                cameraRig.transform.position.z
+            );
+            isJumping = false;
+        }
+    }
+}
