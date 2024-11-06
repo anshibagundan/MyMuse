41fe05960d6e88e8fef85ff4859c38067d64832a
zono0013 <zono@zonos-Mac.local>
2024-11-07 00:12:48 +0900

    デプロイテスト22

--- server/internal/interface/middleware/cors.go
+++ server/internal/interface/middleware/cors.go
@@ -1,24 +1,29 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	"time"
 )
 
 // CORSミドルウェア
 func CORS() gin.HandlerFunc {
+	allowedOrigin := os.Getenv("ALLOWED_ORIGIN") // 環境変数でオリジンを管理
+
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "https://my-muse-pink.vercel.app/") // 許可するオリジン
+		c.Header("Access-Control-Allow-Origin", allowedOrigin)
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, X-Requested-With")
 		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Max-Age", "100")
+		c.Header("Access-Control-Max-Age", fmt.Sprintf("%d", int(time.Hour.Seconds()*24))) // 24時間キャッシュ
 
 		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusOK)
 			return
 		}
 
-		c.Next() // 次のハンドラーを呼び出す
+		c.Next()
 	}
 }
