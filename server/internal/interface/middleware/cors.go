89d56939c01d96b6aeb2a32bea654cd52d560587
zono0013 <zono@zonos-Mac.local>
2024-11-07 02:07:23 +0900

    デプロイテスト28

--- server/internal/interface/middleware/cors.go
+++ server/internal/interface/middleware/cors.go
@@ -1,23 +1,23 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"os"
-	"time"
 )
 
 // CORSミドルウェア
 func CORS() gin.HandlerFunc {
-	allowedOrigin := os.Getenv("ALLOWED_ORIGIN") // 環境変数でオリジンを管理
-
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", allowedOrigin)
+		origin := c.Request.Header.Get("Origin")
+
+		// ローカルと本番のオリジンを許可
+
+		c.Header("Access-Control-Allow-Origin", origin)
+
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, X-Requested-With")
 		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Max-Age", fmt.Sprintf("%d", int(time.Hour.Seconds()*24))) // 24時間キャッシュ
+		c.Header("Access-Control-Max-Age", "86400")
 
 		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusOK)
