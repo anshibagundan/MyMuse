ae78afda6756173db194ea71f2182b89195a221d
zono0013 <zono@zonos-Mac.local>
2024-11-06 19:11:21 +0900

    デプロイテスト16

--- server/internal/usecase/user_usecase.go
+++ server/internal/usecase/user_usecase.go
@@ -71,6 +71,7 @@ func (u *userUseCase) Login(ctx context.Context, input input.User) (*output.User
 		if err := u.userRepo.Create(ctx, user); err != nil {
 			return nil, err
 		}
+		user, err = u.userRepo.FindByID(ctx, input.Email)
 	}
 
 	return &output.UserOutput{
